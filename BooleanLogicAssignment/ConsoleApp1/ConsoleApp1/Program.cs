44073cfd2c7a827a0ee5dbf35f967c7cf62a89ab
HebronShanko <hebronshanko@gmail.com>
2021-10-21 01:55:02 -0700

    assignment
    commit

--- BooleanLogicAssignment/ConsoleApp1/ConsoleApp1/Program.cs
+++ BooleanLogicAssignment/ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,25 +11,34 @@ namespace BooleanLogic
             //int hasSpeedingTickets = 3;
 
             Console.WriteLine("What is your age?");
-            int age = int.Parse(Console.ReadLine());
+            int age = Convert.ToInt32(Console.ReadLine());
 
             //Console.WriteLine("Have you ever had a DUI?");
             //bool hasDUI = true;Console.WriteLine(hasDUI);
 
 
-            Console.WriteLine("Have you ever had a DUI? Please answer with True or False");
-            bool hasDUI = false;
-            string DUIStatus = Convert.ToString(hasDUI);
-            Console.ReadLine();
+            Console.WriteLine("Have you ever had a DUI? Please answer with true or false");
+            bool hasDUI = Convert.ToBoolean(Console.ReadLine());
+            
 
             Console.WriteLine("How many speeding tickets do you have?");
-            int hasSpeedingTickets = int.Parse(Console.ReadLine());
+            int hasSpeedingTickets = Convert.ToInt32(Console.ReadLine());
             
 
             Console.WriteLine("Approved?");
-            bool insuraceApproved = (age >= 15 && !hasDUI && hasSpeedingTickets <= 3);
-            Console.WriteLine(insuraceApproved);
+            if (age >= 16 && hasDUI == false && hasSpeedingTickets <= 3)
+            {
+                Console.WriteLine("You are approved");
+            }
+
+            else
+            {
+                Console.WriteLine("Not Approved");
+
+            }
+
             Console.ReadLine();
+            
 
         }
     }
--- BranchingSubmissionAssignment/BranchingAssignment/BranchingAssignment/Program.cs
+++ BranchingSubmissionAssignment/BranchingAssignment/BranchingAssignment/Program.cs
@@ -19,31 +19,40 @@ namespace BranchingAssignment
             {
                 Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");              
             }
-            
-            Console.ReadLine();
 
-            Console.WriteLine("Please enter the package width:");
-            int packageWidth = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Please enter the package height:");
-            int packageHeight = Convert.ToInt32(Console.ReadLine()); 
-
-            Console.WriteLine("Please enter the package lenght:");
-            int packageLenght = Convert.ToInt32(Console.ReadLine());
-
-
-            int totalDemensions = (packageWidth + packageLenght + packageHeight);
-            if (totalDemensions > 50)
+            else
             {
-                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+
+
+                Console.WriteLine("Please enter the package width:");
+                int packageWidth = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please enter the package height:");
+                int packageHeight = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please enter the package lenght:");
+                int packageLenght = Convert.ToInt32(Console.ReadLine());
+
+
+                int totalDemensions = (packageWidth + packageLenght + packageHeight);
+
+                if (totalDemensions > 50)
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express. Have a good day.");
+                }
+
+                else
+                {
+                    int product = (packageWidth * packageHeight * packageLenght) * packageWeight;
+                    decimal estimatedQuote = Convert.ToDecimal(product / 100);
+
+                    Console.WriteLine("This is how much it costs to ship $ " + estimatedQuote);
+                }
+
+              
             }
 
-            int product = (packageWidth * packageHeight * packageLenght)* packageWeight;
-            decimal estimatedQuote = Convert.ToDecimal(product / 100);
-
-            Console.WriteLine("This is how much it costs to ship $ " + estimatedQuote);
-
-            Console.ReadLine();
+              Console.ReadLine();
             
 
 
