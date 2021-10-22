7ce243f6c90cf12cdab9d1964f672df78db5dcaf
HebronShanko <hebronshanko@gmail.com>
2021-10-21 19:08:51 -0700

    assignmnet
    commit

---
+++ ConsoleAppAssignment/ConsoleAppLoops/ConsoleAppLoops/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 9;
+
+
+            do
+            {
+                switch (number)
+                {
+                    case 10:
+                        Console.WriteLine("You guessed 10, Try again");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    case 40:
+                        Console.WriteLine("You guessed 40, Try again");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 60:
+                        Console.WriteLine("You guessed 60, Try again");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 9:
+                        Console.WriteLine("You guessed 9, CORRECT!");
+                        isGuessed = true;
+                        break;
+                    case 50:
+                        Console.WriteLine("You guessed 50, Try again");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    default:
+                        Console.WriteLine("You are Wrong");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            while (!isGuessed);
+           
+            Console.ReadLine();
+        }
+    }
+}
