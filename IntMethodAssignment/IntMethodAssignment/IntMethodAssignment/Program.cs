534572c3e3c79b0317c8d91f6a54954ccd74e109
HebronShanko <hebronshanko@gmail.com>
2021-10-29 18:55:37 -0700

    assignment
    commit

---
+++ IntMethodAssignment/IntMethodAssignment/IntMethodAssignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a second number");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            TwoInt myMath = new TwoInt();
+            if (y == 0)
+            {
+                int result = myMath.Subtraction(x);
+                Console.WriteLine(result);
+
+            }
+            else
+            {
+                int result = myMath.Subtraction(x, y);
+                Console.WriteLine(result);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ IntMethodAssignment/IntMethodAssignment/IntMethodAssignment/TwoInt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntMethodAssignment
+{
+    class TwoInt
+    {
+        public int Subtraction(int x, int y = 23)
+        {
+            return x - y;
+        }
+    }
+}
