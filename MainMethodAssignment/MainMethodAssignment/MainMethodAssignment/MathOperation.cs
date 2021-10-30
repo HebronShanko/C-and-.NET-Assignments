7da99ab9a9a3a390238b427af9de1469d12306d3
HebronShanko <hebronshanko@gmail.com>
2021-10-29 18:35:38 -0700

    assignment
    commit

---
+++ MainMethodAssignment/MainMethodAssignment/MainMethodAssignment/MathOperation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class MathOperation
+    {
+        public int Subtraction(int x)
+        {
+            return x - 4;
+        }
+        public int Subtraction(decimal p)
+        {
+            int result = Convert.ToInt32(p);
+            return result - 12;
+        }
+        
+        public int Subtraction(string z)
+        {
+            int A = Convert.ToInt32(z);
+            return A - 5;
+        }
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperation a = new MathOperation();
+            int x = a.Subtraction(42);
+            double p = a.Subtraction(24.7m);
+            int z = a.Subtraction("10");
+
+            Console.WriteLine(x);
+            Console.WriteLine(p);
+            Console.WriteLine(z);
+
+            Console.ReadLine();
+        }
+    }
+}
