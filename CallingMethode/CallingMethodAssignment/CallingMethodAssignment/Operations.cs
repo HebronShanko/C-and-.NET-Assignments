6e33087bbe7b6ab706e903b13482d3ee1b6f5c1f
HebronShanko <hebronshanko@gmail.com>
2021-10-29 17:51:39 -0700

    assignment
    commit

---
+++ CallingMethode/CallingMethodAssignment/CallingMethodAssignment/Operations.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodAssignment
+{
+   public class Operations
+    {
+        public int Addition(int x)
+        {
+            return x + 100;
+        }
+        public int Subtraction(int x)
+        {
+            return x - 1;
+        }
+        public int Multiplication (int x)
+        {
+            return x * 2;
+        }
+    }
+}
---
+++ CallingMethode/CallingMethodAssignment/CallingMethodAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int Input = Convert.ToInt32(Console.ReadLine());
+
+            Operations doOperation = new Operations();
+
+            int resultAdd = doOperation.Addition(Input);
+            int resultSubtract = doOperation.Subtraction(Input);
+            int resultMultiply = doOperation.Multiplication(Input);
+
+
+            Console.WriteLine(resultAdd);
+            Console.WriteLine(resultSubtract);
+            Console.WriteLine(resultMultiply);
+
+            Console.ReadLine();
+        }
+    }
+}
