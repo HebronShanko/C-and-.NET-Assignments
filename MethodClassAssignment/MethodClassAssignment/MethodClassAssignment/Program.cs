0942745dab6ce1e342e2d2f87cfe3b0cdd6f8d30
HebronShanko <hebronshanko@gmail.com>
2021-10-30 17:33:09 -0700

    assignment
    commit

---
+++ MethodClassAssignment/MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MultiOperation a = new MultiOperation();
+
+            a.Times(20, 5);
+            a.Times(num1: 10, num2: 10);
+            Console.ReadLine();
+        }
+    }
+}
