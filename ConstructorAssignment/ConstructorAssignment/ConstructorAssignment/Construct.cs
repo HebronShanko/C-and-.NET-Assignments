c8b194589c8a345965f7543035a29fe399b5ebc7
HebronShanko <hebronshanko@gmail.com>
2021-11-03 08:05:47 -0700

    assignment
    commit

---
+++ ConstructorAssignment/ConstructorAssignment/ConstructorAssignment/Construct.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    class Construct
+    {
+        public class ChainConstruct
+        {
+            public ChainConstruct() : this(10)
+            {
+
+            }
+            public ChainConstruct(int Age)
+            {
+
+            }
+        }
+    }
+}
---
+++ ConstructorAssignment/ConstructorAssignment/ConstructorAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string MovieName = "Malcolm X";
+            var i = 13;
+
+            Console.WriteLine("Welcome to {0} lets see the movie", MovieName);
+
+            Console.WriteLine(i);
+        }
+    }
+}
