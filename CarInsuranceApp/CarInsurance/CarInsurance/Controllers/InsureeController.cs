785532b5e19c064490fd23f382e4c65698c42918
HebronShanko <hebronshanko@gmail.com>
2021-11-13 16:05:33 -0800

    commit
    assignment

---
+++ CarInsuranceApp/CarInsurance/CarInsurance/Controllers/InsureeController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CarInsurance.Models;
+
+namespace CarInsurance.Controllers
+{
+    public class InsureeController : Controller
+    {
+        private InsuranceEntities db = new InsuranceEntities();
+
+        // GET: Insuree
+        public ActionResult Index()
+        {
+            return View(db.Insurances.ToList());
+        }
+
+        // GET: Insuree/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insurance insurance = db.Insurances.Find(id);
+            if (insurance == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insurance);
+        }
+
+        // GET: Insuree/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Insuree/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Insurance insurance)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Insurances.Add(insurance);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(insurance);
+        }
+
+        // GET: Insuree/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insurance insurance = db.Insurances.Find(id);
+            if (insurance == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insurance);
+        }
+
+        // POST: Insuree/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Insurance insurance)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(insurance).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(insurance);
+        }
+
+        // GET: Insuree/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insurance insurance = db.Insurances.Find(id);
+            if (insurance == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insurance);
+        }
+
+        // POST: Insuree/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Insurance insurance = db.Insurances.Find(id);
+            db.Insurances.Remove(insurance);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Console.NetCore/Scores/Scores/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Scores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your first name");
+            string date = DateTime.Today.ToShortDateString();
+            string uName = Console.ReadLine();
+            string msg = $"\nWelcome Back {uName}. Today is {date}.  ";
+            Console.WriteLine(msg);
+
+            string path = @"C:\Users\hebro\OneDrive\Documents\GitHub\C-and-.NET-Assignments\Console.NetCore\Scores\Scores\studentScores";
+            string[] lines = System.IO.File.ReadAllLines(path);
+
+
+            double tScore = 0.0;
+
+
+            Console.WriteLine("\nStudnet Scores: \n");
+            foreach (string line in lines)
+            {
+                Console.WriteLine("\n + line");
+                double score = Convert.ToDouble(line);
+                tScore += score;
+            }
+
+            double avgScore = tScore / lines.Length;
+            Console.WriteLine("\nTotoal of " + lines.Length + " student scores. \tAverage score: " + avgScore);
+
+
+
+
+            Console.WriteLine("\n\nPress any key to exist. ");
+            Console.ReadLine();
+
+        }
+    }
+}
